b1ff98dd49f037b4f6bf27dfa9f21be6e78f680c
Amazigh Lasri <amlasri@f2r3s16.clusters.42paris.fr>
2026-01-22 18:16:48 +0100

    push

--- parse_av.cpp
+++ parse_av.cpp
@@ -29,16 +29,60 @@ void ft_main_socket(server serv)
 	sin.sin_port = htons(serv.getport());
 	inet_aton("127.0.0.1", &sin.sin_addr);
 
-
 	n = socket(AF_INET, SOCK_STREAM, 0);
 	bind(n, (struct sockaddr*)&sin, sizeof(sin));
+	listen(n, SOMAXCONN);
 
 	std::vector<pollfd> fds;
+	pollfd serv_fds;
+	serv_fds.fd = n;
+	serv_fds.events = POLLIN;
+	fds.push_back(serv_fds); 
+	std::cout << "Serveur TCP démarré sur le port " << serv.getport() << std::endl;
 	while (1)
 	{
 		int ret = poll(fds.data(), fds.size(), -1);
 		if (ret < 0)
 			throw std::runtime_error("error: poll");
-		
+		for (size_t i = 0; i < fds.size(); i++)
+		{
+			if (fds[i].fd == n && (fds[i].revents & POLLIN))
+			{
+				sockaddr_in client_addr;
+				socklen_t client_len = sizeof(client_addr);
+				int client_fd = accept(n, (sockaddr*)&client_addr, &client_len);
+				if (client_fd < 0) 
+                    throw std::runtime_error("error: accept");
+				fcntl(client_fd, F_SETFL, O_NONBLOCK);
+				pollfd client_pollfd;
+                client_pollfd.fd = client_fd;
+                client_pollfd.events = POLLIN;
+                fds.push_back(client_pollfd);
+                std::cout << "Nouveau client connecté, fd=" << client_fd << std::endl;
+			}
+			else if (fds[i].revents & POLLIN)
+			{
+				char buffer[1024];
+				ssize_t bytes = recv(fds[i].fd, buffer, sizeof(buffer) - 1, 0);
+				if (bytes <= 0)
+				{
+					std::cout << "Client fd=" << fds[i].fd << " déconnecté." << std::endl;
+					close(fds[i].fd);
+					fds.erase(fds.begin() + i);
+					i--;
+					continue;
+				}
+				buffer[bytes] = '\0';
+				std::cout << "Reçu de fd=" << fds[i].fd << ": " << buffer << std::endl;
+				send(fds[i].fd, buffer, bytes, 0);
+			}
+			else if (fds[i].revents & (POLLHUP | POLLERR))
+			{
+				std::cout << "Erreur sur fd=" << fds[i].fd << std::endl;
+				close(fds[i].fd);
+				fds.erase(fds.begin() + i);
+				i--;
+			}
+		}
 	}
 }
