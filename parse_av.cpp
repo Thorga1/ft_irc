e4b2742f1791d009a709a3dbe203755eace1125f
Leo Garcia <leogarci@f2r4s19.clusters.42paris.fr>
2026-01-22 16:43:51 +0100

    parsing

--- parse_av.cpp
+++ parse_av.cpp
@@ -7,12 +7,13 @@ void	parse_av(char **av)
 		{
 			if (!std::isdigit(av[1][i]))
 				throw std::runtime_error("error: <port> contains not only digits");
-			if (av[2][0])
-			 ;
+			if (!av[2][0])
+				throw std::runtime_error("error: no password been set");
 		}
 
 	else
 		throw std::runtime_error("invalid <port>");
 	server serv(av[2], (unsigned int)std::atoi(av[1]));
-	std::cout << serv.getport() << std::endl;
+	if (serv.getport() < 1024 || serv.getport() > 65535)
+		throw std::runtime_error("error: port not valid");
 }
